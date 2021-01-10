b1becf53812a745caa979e6eb2194bf29ae04f12
Sunjingwen <453129077@qq.com>
2021-01-10 14:25:51 +0800

    导航

--- miniprogram/miniprogram/pages/one/one.js
+++ miniprogram/miniprogram/pages/one/one.js
@@ -1,32 +1,19 @@
-// miniprogram/pages/index/index.js
+// miniprogram/pages/one/one.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    msg: '今天是个好日子',
-    arr: ['a', 'b', 'c', 'd', 'e'],
-    num: 0,
-    objArr: [
-      {name:'zhangsan',age:23},
-      {name:'lisi',age:14},
-      {name:'wangwu',age:56},
-      {name:'xiaoming',age:78},
-      {name:'xiaohong',age:99},
-    ]
-  },
-  goOne() {
-    wx.navigateTo({
-      url: '../one/one?id=11111',
-    })
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // 通过 option 来获得页面的参数
+    console.log(options);
   },
 
   /**
@@ -61,17 +48,14 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    console.log('用户下拉了，要刷新数据');
-    setTimeout(() => {
-      wx.stopPullDownRefresh();
-    }, 100);
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    console.log('到底了，要加载新数据了');
+
   },
 
   /**
