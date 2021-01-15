b1bb22ab72eb66125db503762d51ab11e2e7d63a
Sunjingwen <453129077@qq.com>
2021-01-15 13:46:54 +0800

    小程序

--- miniprogram/miniprogram/pages/two/two.js
+++ miniprogram/miniprogram/pages/two/two.js
@@ -20,7 +20,16 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    wx.request({
+      url: 'https://m.douban.com/rexxar/api/v2/skynet/playlists?from_rexxar=true', //仅为示例，并非真实的接口地址
+      data: {
+        x: '',
+        y: ''
+      },
+      success (res) {
+        console.log(res.data)
+      }
+    })
   },
 
   /**
